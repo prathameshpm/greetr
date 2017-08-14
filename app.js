b660768a2296a3022786849bbdf7354059a9e52e
prathameshpm <mahamulkarprathamesh@gmail.com>
2017-08-14 12:11:41 +0530

    Added properties and chainable methods

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-var g = G$('John');
-console.log(g);
+// Gets a new object (the architecture allows us to not have to use the 'new' keyword here)
+var g = G$('Jane', 'Smith');
+
+// Use our chainable methods
+g.greet().setLang('es').setGend('f').greet().log();
+g.greet().setLang('en').setGend('f').greet().log();
--- greetr.js
+++ greetr.js
@@ -4,10 +4,107 @@
     // 'new' Object
     var Greetr = function (firstName, lastName, gender, language) {
         return new Greetr.init(firstName, lastName, gender, language);
-    }
+    };
+
+    // Hidden within the scope of IIFE never accessible
+    var supportedLangs = ['en', 'es'];
+
+    // Gender
+    var gendr = ['m', 'f'];
+
+    // Greetings
+    var greetings = {
+        en: 'Hello',
+        es: 'Hola',
+        m: 'sir',
+        f: 'madam'
+    };
+
+    // Logger messages
+    var logMessages = {
+        m: {
+            en: 'You are Logged in sir',
+            es: 'Has iniciado sesión sir'
+        },
+        f: {
+            en: 'You are Logged in madam',
+            es: 'Está accedido a madam'
+        }
+
+    };
 
     // Prototype holds methods (helps save memory space)
-    Greetr.prototype = {};
+    Greetr.prototype = {
+
+        validate: function () {
+            // Check that it is a valid language and gender
+            // References the externally inaccessible supportedLangs and gendr within the closure
+            if (supportedLangs.indexOf(this.language) === -1) {
+                throw "Invalid Language";
+            }
+            if (gendr.indexOf(this.gender) === -1) {
+                throw "Invalid Gender";
+            }
+        },
+
+        // 'this' refers to the calling object at execution line
+        fullName: function () {
+            return this.firstName + ' ' + this.lastName;
+        },
+
+        // Retrieve messages from the object by referring to properties using [] syntax
+        greeting: function () {
+            return greetings[this.language] + ' ' + this.fullName() + ' ' + greetings[this.gender];
+        },
+
+        // Chainable methods return their own containing object
+        greet: function () {
+            var msg = this.greeting();
+
+            if (console) {
+                console.log(msg);
+            }
+
+            // 'this' refers to the calling object at execution line
+            // makes the method chainable
+            return this;
+        },
+
+        log: function () {
+            if (console) {
+                console.log(logMessages[this.gender][this.language] + ': ' + this.fullName());
+                console.log('-----------------------------------------------------------');
+            }
+
+            // makes chainable
+            return this;
+        },
+
+        setLang: function (lang) {
+
+            // Set the language
+            this.language = lang;
+
+            // Validate
+            this.validate();
+
+            // Make chainable
+            return this;
+        },
+
+        setGend: function (gend) {
+
+            // Set the Gender
+            this.gender = gend;
+
+            // Validate
+            this.validate();
+
+            // Make chainable
+            return this;
+        }
+
+    };
 
     // The actual object is created here, allowing us to use 'new' object without calling new
     Greetr.init = function (firstName, lastName, gender, language) {
@@ -17,6 +114,7 @@
         self.lastName = lastName || '';
         self.gender = gender || 'm';
         self.language = language || 'en';
+        self.validate();
 
     }
 
